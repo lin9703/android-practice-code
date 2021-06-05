c301086d7dd898930f0b3f3933fdd7b982b82c3c
김효린 <hyorin97@naver.com>
2021-06-05 19:16:22 +0900

    feat: add next button event

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -27,6 +30,8 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             Toast.makeText(
@@ -45,5 +50,18 @@ class MainActivity : AppCompatActivity() {
             )
                 .show()
         }
+
+        nextButton.setOnClickListener { view: View ->
+            currentIndex = ++currentIndex % questionBank.size
+
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
