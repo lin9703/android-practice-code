6cffd14c44609c52641c0e0512a0c20244c37a20
김효린 <hyorin97@naver.com>
2021-06-05 18:32:25 +0900

    feat: Add event listener about quiz button

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,23 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
     }
 }
